4c061dd308e91a9d190782f1327af37870bcaed3
LapTapk <fret4657@gmail.com>
2026-02-16 01:21:49 +0300

    New: add `bind` syscall handling for UNIX sockets
    Other types of sockets can be listened by other tools like wireshark

--- src/tracer.rs
+++ src/tracer.rs
@@ -1,28 +1,88 @@
 use anyhow::Result;
 use nix::sys::ptrace;
 use nix::sys::ptrace::Options;
+use nix::sys::signal::Signal;
+use nix::sys::uio::{RemoteIoVec, process_vm_readv};
 use nix::sys::wait::{WaitStatus, waitpid};
 use nix::unistd;
+use std::collections::HashSet;
+use std::ffi::CStr;
+use std::io::IoSliceMut;
 use std::sync::OnceLock;
 use tokio::{sync::mpsc, task::JoinHandle, task::JoinSet};
 
 static TX: OnceLock<mpsc::UnboundedSender<unistd::Pid>> = OnceLock::new();
 static SOCK: OnceLock<Option<String>> = OnceLock::new();
 
+const SECCOMP_EVENT: i32 = Signal::SIGTRAP as i32 | ptrace::Event::PTRACE_EVENT_SECCOMP as i32;
+
 struct Tracer {
     pid: unistd::Pid,
+    track_fds: HashSet<i32>,
 }
 
 impl Tracer {
     pub fn new(pid: unistd::Pid) -> Self {
-        Self { pid }
+        Self {
+            pid,
+            track_fds: HashSet::new(),
+        }
     }
 
-    fn handle_ev(&self, ev: i32) -> Result<()> {
+    fn read_unix_path(&self, sockaddr_ptr: usize) -> Result<String> {
+        let mut sockaddr_u8_arr: [u8; 108] = [0 as u8; 108];
+        let sockaddr_ioslice = IoSliceMut::new(&mut sockaddr_u8_arr);
+        let sockaddr_rem = RemoteIoVec {
+            base: sockaddr_ptr + 4,
+            len: 108,
+        };
+        process_vm_readv(self.pid, &mut [sockaddr_ioslice], &[sockaddr_rem])?;
+        let cstr = CStr::from_bytes_with_nul(&sockaddr_u8_arr).unwrap();
+        let s = cstr.to_str().unwrap();
+        Ok(s.to_string())
+    }
+
+    fn handle_bind(&mut self, regs: libc::user_regs_struct) -> Result<()> {
+        let mut family_u8_arr: [u8; 4] = [0 as u8; 4];
+        let family_ioslice = IoSliceMut::new(&mut family_u8_arr);
+        let family_rem = RemoteIoVec {
+            base: regs.rsi as usize,
+            len: 4,
+        };
+        process_vm_readv(self.pid, &mut [family_ioslice], &[family_rem])?;
+
+        let family = i32::from_ne_bytes(family_u8_arr);
+
+        if family != libc::AF_UNIX {
+            return Ok(());
+        }
+
+        let sockaddr = self.read_unix_path(regs.rsi as usize)?;
+        let target_sock = SOCK.get().unwrap();
+        if target_sock.as_ref().is_none_or(|s| &sockaddr == s) {
+            self.track_fds.insert(regs.rdi as i32);
+        }
+
         Ok(())
     }
 
-    fn _trace(&self) -> Result<()> {
+    fn handle_seccomp(&mut self) -> Result<()> {
+        let regs = ptrace::getregs(self.pid)?;
+        match regs.rax as i64 {
+            libc::SYS_bind => self.handle_bind(regs)?,
+            _ => return Err(anyhow::Error::msg("Unregistered syscall")),
+        }
+        Ok(())
+    }
+
+    fn handle_ev(&mut self, ev: i32) -> Result<()> {
+        match ev >> 8 {
+            SECCOMP_EVENT => self.handle_seccomp(),
+            _ => Ok(()),
+        }
+    }
+
+    fn _trace(&mut self) -> Result<()> {
         ptrace::seize(
             self.pid,
             Options::PTRACE_O_TRACESYSGOOD
@@ -45,7 +105,7 @@ impl Tracer {
         }
     }
 
-    pub fn trace(&self) {
+    pub fn trace(&mut self) {
         if let Err(e) = self._trace() {
             eprint!("[PID: {}]", self.pid);
             crate::log_err!(e);
