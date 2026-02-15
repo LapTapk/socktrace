55041c7e9749a010a34011b462688faae42aa69d
LapTapk <fret4657@gmail.com>
2026-02-15 21:30:02 +0300

    Refator tracee
    Add add_rules! macro to eliminate code copying

--- src/tracee.rs
+++ src/tracee.rs
@@ -4,44 +4,38 @@ use nix::sys::signal;
 use nix::unistd;
 use std::ffi::CString;
 
+macro_rules! add_rules {
+    ($filter:expr => $( $syscall:expr $(,)? )* ) => {
+       $(
+            $filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name($syscall)?)?;
+       )*
+    }
+}
+
 fn install_filter() -> Result<()> {
     let mut filter = ScmpFilterContext::new(ScmpAction::Allow)?;
 
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("sendmsg")?)?;
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("recvmsg")?)?;
-
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("sendto")?)?;
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("recvfrom")?)?;
-
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("send")?)?;
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("recv")?)?;
-
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("read")?)?;
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("write")?)?;
-
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("readv")?)?;
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("writev")?)?;
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("preadv")?)?;
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("pwritev")?)?;
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("preadv")?)?;
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("preadv2")?)?;
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("pwritev2")?)?;
-
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("sendfile")?)?;
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("splice")?)?;
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("vmsplice")?)?;
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("tee")?)?;
-    filter.add_rule(
-        ScmpAction::Trace(0),
-        ScmpSyscall::from_name("copy_file_range")?,
-    )?;
-
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("bind")?)?;
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("connect")?)?;
-
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("dup")?)?;
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("dup2")?)?;
-    filter.add_rule(ScmpAction::Trace(0), ScmpSyscall::from_name("dup3")?)?;
+    add_rules!(filter =>
+        "sendmsg",
+        "recvmsg", 
+        "sendto",
+        "recvfrom",
+        "send", 
+        "recv",
+        "read",
+        "write",
+        "readv",
+        "writev",
+        "preadv",
+        "pwritev",
+        "preadv2",
+        "pwritev2",
+        "sendfile",
+        "splice",
+        "vmsplice",
+        "tee", 
+        "copy_file_range"
+    );
 
     filter.load()?;
     Ok(())
