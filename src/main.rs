aaf9c361886a8c122b6285747431baa762f9693a
LapTapk <fret4657@gmail.com>
2026-02-26 19:03:40 +0300

    New: add zombie handling

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use crate::tracee::tracee_procedure;
 use crate::tracer::tracer_procedure;
 use anyhow::Result;
 use clap::Parser;
+use nix::sys::signal::{self, SaFlags, SigAction, SigHandler, SigSet, Signal};
 use nix::unistd;
 use std::fs::create_dir_all;
 use std::path::PathBuf;
@@ -23,10 +24,23 @@ struct Cli {
     sock: Option<String>,
 }
 
+pub fn auto_reap_children() -> nix::Result<()> {
+    let action = SigAction::new(
+        SigHandler::SigIgn,
+        SaFlags::SA_NOCLDWAIT | SaFlags::SA_RESTART,
+        SigSet::empty(),
+    );
+    unsafe { signal::sigaction(Signal::SIGCHLD, &action) }?;
+    unsafe { libc::prctl(libc::PR_SET_PDEATHSIG, libc::SIGKILL) };
+    Ok(())
+}
+
 fn main() -> Result<()> {
     let args = Cli::parse();
 
     create_dir_all(&args.outdir)?;
+
+    auto_reap_children()?;
 
     match unsafe { unistd::fork() } {
         Ok(unistd::ForkResult::Parent { child: _ }) => {
